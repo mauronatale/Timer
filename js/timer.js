c4c3255ca46b7121a1a87f6011c2ac3240775d4f
MauroNatale <mauro.natale00@gmail.com>
2023-01-20 11:12:13 -0300

    TimerJS

---
+++ js/timer.js
@@ -0,0 +1,54 @@
+let inputs, clock, alarm, hours, minutes, seconds, repeater;
+
+window.addEventListener('load',()=>{
+    inputs = Array.from(document.getElementsByClassName('number'));
+    clock = document.querySelector('.clock');
+    alarm = new Audio('sound/fantasy-intro-theme-music.mp3');
+});
+
+function startTimer(){
+    parseTime();
+}
+
+function parseTime (){
+    hours = Number (inputs [0].value);
+    minutes = Number (inputs [1].value);
+    seconds = Number (inputs [2].value);
+}
+
+function SetTimer(){
+    clock.innerHTML = `<p class="number">${hours > 9 ? hours : ('0' + hours)}</p><span>hs</span>
+                       <p class="number">${minutes > 9 ? minutes : ('0' + minutes)}</p><span>min</span>
+                       <p class="number">${seconds > 9 ? seconds : ('0' + seconds)}</p><span>sec</span>`
+                       
+    document.tittle = `${hours > 9 ? hours : ('0' + hours)}: ${minutes > 9 ? minutes : ('0' + minutes)}: ${seconds > 9 ? seconds : ('0' + seconds)}`;
+}
+
+function countdown(){
+    repeater = setInterval (runner, 1000);
+}
+
+function runner(){
+    if (seconds > 0){
+        seconds --;
+    }else{
+        if (minutes > 0){
+            seconds = 59;
+            minutes --;  
+    }else{
+        if (hours > 0){
+            seconds = 59;
+            minutes = 59;
+            hours--;
+        }else{
+            alarm.play();
+            
+        }
+    }
+}
+    SetTimer();
+}
+function stopTimer(){
+    clearInterval(repeater);
+    location.reload()
+}
